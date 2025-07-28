57f2a381a9403ae5937ea33a02bfb8a44c207225
Vellampallilohitha <vvlohitha@gmail.com>
2025-07-28 18:43:18 +0530

    changed

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import fruits from "./fruits.json";
+
+function App() {
+  return (
+    <div>
+      <h1>Fruit Groups</h1>
+
+      {Object.entries(fruits).map(([groupName, fruitList]) => (
+        <div key={groupName}>
+          <h2>{groupName.toUpperCase()}</h2>
+          <ul>
+            {fruitList.map((fruit) => (
+              <li key={fruit.id}>
+                {fruit.name} - ₹{fruit.rate}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
